4ba03a81ebabe0a4ae6ec9bc7e1af7175649a0da
Chris <50994413+chrise01@users.noreply.github.com>
2019-09-03 14:15:42 +0930

    Update Linux-Agent.sh

--- Linux-Agent.sh
+++ Linux-Agent.sh
@@ -1,5 +1,5 @@
 ##https://docs.vmware.com/en/VMware-Horizon-7/7.9/linux-desktops-setup/GUID-92110540-8170-49C3-A150-F9C64D5075DB.html#GUID-92110540-8170-49C3-A150-F9C64D5075DB
-
+##https://docs.vmware.com/en/VMware-Horizon-7/7.9/linux-desktops-setup/GUID-F06FF1A7-BDEF-4269-B2AB-C62819D4FCCD.html
 ##
 #cd ??
 sudo ./vmware-install.pl -d
@@ -10,3 +10,5 @@ sudo apt-get install open-vm-tools-desktop
 ftp admin@VMware1!:192.168.2.30
 get HZ.tar.gz
 quit
+tar -xzvf HZ.tar.gz
+sudo ./install_viewagent.sh 
