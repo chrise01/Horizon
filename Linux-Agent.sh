f0098e233f258c27bdcf3645accef705cdee0eeb
Chris <50994413+chrise01@users.noreply.github.com>
2019-09-04 15:41:01 +0930

    Update Linux-Agent.sh

--- Linux-Agent.sh
+++ Linux-Agent.sh
@@ -17,3 +17,4 @@ tar -xzvf HZ.tar.gz
 sudo ./Desktop/HZ/VMware-horizonagent-linux-x86_64-7.9.0-13916467/install_viewagent.sh -b HZ-Connection.test.local -d test.local -u administrator -k Win16-AD.test.local -  -A yes -M no -r yes
 ##Where -b is the connection broker, -d is the domain of the administrator account and -u is for the administrators user account itself. The -k is for the domain controller where the user account resides (Kerberos).
 ## sudo service viewagent status
+##issues - DNS, check resolution, capitals, add a host file entry - should see it connect in the /var/logs/VMware/viewagent-debug.log - didn't like the "-" in the connection server name
